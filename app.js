f743a26b89f38e1613b101dad4e0fdfd2239cd95
JUAN PABLO REINOSO MARTINEZ <auxiliar.tecnico@usbmed.edu.co>
2024-08-02 16:22:15 -0500

    actualizacion en los estilos de mi formulario

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const frmEmail = document.getElementById('frm-email')
-frmEmail.addEventListener('submit', sendEmail)
+const frmEmail = document.getElementById("frm-email");
+frmEmail.addEventListener("submit", sendEmail);
 
-const serviceId = 'service_qur2pc9'
-const templateId = 'template_ogamv4m'
-const apikey = 'QT54_HA5kv4hiaU6A'
+const serviceId = "service_qur2pc9";
+const templateId = "template_ogamv4m";
+const apikey = "QT54_HA5kv4hiaU6A";
 
-function sendEmail(event){
-    event.preventDefault()
-    emailjs.init(serviceId)
+function sendEmail(event) {
+  event.preventDefault();
+  emailjs.init(serviceId);
 
-    emailjs
-    .sendForm(serviceId,templateId,frmEmail,apikey)
-    .then( (result) => Swal.fire('Su mensaje se envio conrrectamente.'))
-    .catch( (error) => {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'No ha sido posible enviar el mensaje!',
-        });
+  emailjs
+    .sendForm(serviceId, templateId, frmEmail, apikey)
+    .then((result) => Swal.fire("Su mensaje se envio correctamente."))
+    .catch((error) => {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "No ha sido posible enviar el mensaje!",
+      });
     });
-
 }
